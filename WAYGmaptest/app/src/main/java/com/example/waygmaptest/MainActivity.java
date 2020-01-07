96bcdc3a57fe6e2e0a058a5b20e185d9a71a5d22
ohaeori <0typhoon416@gmail.com>
2020-01-07 15:52:15 +0900

    update WAYGmap, meetinglog

--- WAYGmaptest/app/src/main/java/com/example/waygmaptest/MainActivity.java
+++ WAYGmaptest/app/src/main/java/com/example/waygmaptest/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.waygmaptest;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
 
+import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.InfoWindow;
+import com.naver.maps.map.overlay.Marker;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, NaverMap.OnMapClickListener {
+
+    NaverMap Map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         mapFragment.getMapAsync(this);
 
+
     }
 
     @UiThread
@@ -31,10 +40,35 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public  void  onMapReady(NaverMap naverMap){
         naverMap.setMapType(NaverMap.MapType.Basic);
         naverMap.setSymbolScale(1.5f);
+        Map = naverMap;
+
+        naverMap.setOnMapClickListener(this);
     }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
+    }
+
+    @Override
+    public void onMapClick(@NonNull PointF pointF, @NonNull LatLng latLng) {
+        Marker marker1 = new Marker();
+        marker1.setPosition(latLng);
+        marker1.setMap(Map);
+        marker1.setSubCaptionText("????????");
+        marker1.setSubCaptionColor(Color.RED);
+        marker1.setSubCaptionHaloColor(Color.YELLOW);
+        marker1.setSubCaptionTextSize(10);
+
+        InfoWindow infoWindow1 = new InfoWindow();
+        infoWindow1.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                return "!!!!!!!!!!!!";
+            }
+        });
+
+        infoWindow1.open(marker1);
     }
 }
