4d322f279cd75b20230dc00d4e7c9ef39415e50a
ljh7857 <ljh7857@gmail.com>
2020-01-09 18:34:32 +0900

    server update(grade)

--- vacapj/app/src/main/java/com/example/vacapj/Room_Info.java
+++ vacapj/app/src/main/java/com/example/vacapj/Room_Info.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,6 +19,8 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Map;
+
+import static java.sql.Types.NULL;
 
 public class Room_Info extends AppCompatActivity {
 
@@ -40,7 +43,8 @@ public class Room_Info extends AppCompatActivity {
     String uid2;
     String uid3;
     String uid4;
-    String grd="hi";
+    private OnCompleteListener<DocumentSnapshot> onCompleteListener;
+    static String grd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,13 +52,13 @@ public class Room_Info extends AppCompatActivity {
         timeText = findViewById(R.id.output_time);
         payText  = findViewById(R.id.output_pay);
         mem1Text = findViewById(R.id.mem1);
-        mem2Text = findViewById(R.id.mem2);
-        mem3Text = findViewById(R.id.mem3);
-        mem4Text = findViewById(R.id.mem4);
+        //mem2Text = findViewById(R.id.mem2);
+        //mem3Text = findViewById(R.id.mem3);
+        //mem4Text = findViewById(R.id.mem4);
         grade1Text = findViewById(R.id.grade1);
-        grade2Text = findViewById(R.id.grade2);
-        grade3Text = findViewById(R.id.grade3);
-        grade4Text = findViewById(R.id.grade4);
+        //grade2Text = findViewById(R.id.grade2);
+        //grade3Text = findViewById(R.id.grade3);
+        //grade4Text = findViewById(R.id.grade4);
 
 
         db= FirebaseFirestore.getInstance();
@@ -64,8 +68,8 @@ public class Room_Info extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 if(task.isSuccessful()){
-                    String timeString = "";
-                    String payString = "";
+                    String timeString;
+                    String payString;
 
                     DocumentSnapshot document = task.getResult();
                     Map<String,Object> map = document.getData();
@@ -76,21 +80,34 @@ public class Room_Info extends AppCompatActivity {
                     payString = (String)map.get("pay");
                     timeText.setText(timeString);
                     payText.setText(payString);
-                    uid1 = arrayList.get(0);
+                    /*uid1 = arrayList.get(0);
                     mem1Text.setText(uid1);
                     uidRead(uid1);
-                    //grade1Text.setText(uidRead(uid1)); 왜 grd 를 함수로 넘기는건 안되지?
+                    System.out.println(grd);
+                    grade1Text.setText(grd); //왜 grd 를 함수로 넘기는건 안되지?
                     if(mem_Number>1) {
                         uid2 = arrayList.get(1);
                         mem2Text.setText(uid2);
+                        uidRead(uid2);
+                        grade2Text.setText(grd);
                         if(mem_Number>2) {
                             uid3 = arrayList.get(2);
                             mem3Text.setText(uid3);
-                            if(mem_Number>3)
+                            uidRead(uid3);
+                            grade3Text.setText(grd);
+                            if(mem_Number>3) {
                                 uid4 = arrayList.get(3);
                                 mem4Text.setText(uid4);
+                                uidRead(uid4);
+                                grade4Text.setText(grd);
+                            }
                         }
-                    }
+                    }*/
+                    for(int i = 0;i<arrayList.size();i++)
+                        if(arrayList.indexOf(i)!=NULL) {
+                            mem1Text.setText(mem1Text.getText().toString() + "\n\n\n" + arrayList.get(i));
+                            uidRead(arrayList.get(i));
+                        }
                     if (document.exists()) {
                         Log.d(TAG, "DocumentSnapshot data: " + document.getData());
                     } else {
@@ -104,22 +121,31 @@ public class Room_Info extends AppCompatActivity {
         });
 
     }//여기서부터 아이디정보 기반으로 평점을 불러와야하는데...
-    public String uidRead (String uid){
+    public void setGrd(String g)
+    {
+        Room_Info.grd = g;
+        //this.grd = g;
+    }
+
+    public void uidRead (String uid){
 
         docRef = db.collection("Users").document(uid);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            String gg;
+
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
-
                     Map<String, Object> map1 = document.getData();
                     long sum =  (long)map1.get("gradesum");
                     long num =  (long)map1.get("gradenum");
                     double grade = (double)sum / (double)num;
-                    grd = Double.toString(Math.round(grade*100)/100.0);
-                    grade1Text.setText(grd);
+                    gg = Double.toString(Math.round(grade*100)/100.0);
+                    setGrd(gg);
+                    Room_Info.grd = gg;
+                    grade1Text.setText(grade1Text.getText().toString()+"\n\n\n"+gg);
+                    SystemClock.sleep(5);
                     if (document.exists()) {
                         Log.d(TAG, "DocumentSnapshot data: " + document.getData());
 
@@ -133,7 +159,11 @@ public class Room_Info extends AppCompatActivity {
             }
 
         });
-        return grd;
+
     }
 
 }
+/*
+수정해야할 부분: 방정보를 익명클래스에서 열어 그 내부에서 유저정보를 다시 접근하는데 평점 순서가 뒤죽박죽.
+얼핏 보기에는 스레드 꼬이는거랑 비슷해 보이기는 하는데...
+ */
