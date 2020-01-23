ee779707bac6c34df4f6217e17a83b998928929e
ohaeori <0typhoon416@gmail.com>
2020-01-23 13:57:55 +0900

    update DEMO

--- WAYGDEMO/app/src/main/java/com/example/waygdemo/NickNameActivity.java
+++ WAYGDEMO/app/src/main/java/com/example/waygdemo/NickNameActivity.java
@@ -21,12 +21,14 @@ public class NickNameActivity extends Activity {
     TextView state_message;
     EditText nickname;
     Button create_button;
-
+    LoginActivity loginActivity;
+    String email;
+    String type;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nick_name);
-        final LoginActivity loginActivity = (LoginActivity)LoginActivity.loginActivity;
+        loginActivity = (LoginActivity)LoginActivity.loginActivity;
 
         user_id = (TextView)findViewById(R.id.activity_nickname_user_id);
         state_message = (TextView)findViewById(R.id.activity_nickname_state);
@@ -35,43 +37,12 @@ public class NickNameActivity extends Activity {
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
-        String email = bundle.getString("email");
-        final String type = bundle.getString("type");
+        email = bundle.getString("email");
+        type = bundle.getString("type");
 
         user_id.setText(email);
 
-        create_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(create_button.getText().toString().equals("생성")){
-                    /*save in database*/
-
-                    /*move on next intent*/
-                    loginActivity.finish();
-                    NickNameActivity.this.finish();
-                    Intent intent = new Intent(NickNameActivity.this, MapActivity.class);
-                    intent.putExtra("email", email);
-                    intent.putExtra("nickname", nickname.getText().toString());
-                    intent.putExtra("type",type);
-                    startActivity(intent);
-                }
-                else if(nickname.getText().toString().equals("")) {
-                    state_message.setTextColor(getResources().getColor(R.color.holo_red_light));
-                    state_message.setText("유효하지 않은 닉네임입니다.");
-                    return;
-                }
-                else if(nickname.getText().toString().equals("존재")){
-                    state_message.setTextColor(getResources().getColor(R.color.holo_red_light));
-                    state_message.setText("이미 존재하는 닉네임입니다.");
-                    return;
-                }
-                else{
-                    state_message.setTextColor(getResources().getColor(R.color.holo_green_light));
-                    state_message.setText("사용 가능한 닉네임입니다.");
-                    create_button.setText("생성");
-                }
-            }
-        });
+        create_button.setOnClickListener(mOnClickListener);
 
         nickname.addTextChangedListener(new TextWatcher() {
             @Override
@@ -93,12 +64,48 @@ public class NickNameActivity extends Activity {
             }
         });
 
-        /*back event*/
+
+    }
+
+    View.OnClickListener mOnClickListener = new View.OnClickListener() {
         @Override
-        public void onBackPressed() {
-            super.onBackPressed();
-            if (type.equals("NAVER")) mOAuthLoginModule.logout(mContext);
-            else if (type.equals("GOOGLE")) mGoogleSignInClient.signOut();
+        public void onClick(View v) {
+            if(create_button.getText().toString().equals("생성")){
+                /*save in database*/
+
+                /*move on next intent*/
+                loginActivity.finish();
+                NickNameActivity.this.finish();
+                Intent intent = new Intent(NickNameActivity.this, MapActivity.class);
+                intent.putExtra("email", email);
+                intent.putExtra("nickname", nickname.getText().toString());
+                intent.putExtra("type",type);
+                startActivity(intent);
+            }
+            else if(nickname.getText().toString().equals("")) {
+                state_message.setTextColor(getResources().getColor(R.color.holo_red_light));
+                state_message.setText("유효하지 않은 닉네임입니다.");
+                return;
+            }
+            else if(nickname.getText().toString().equals("존재")){
+                state_message.setTextColor(getResources().getColor(R.color.holo_red_light));
+                state_message.setText("이미 존재하는 닉네임입니다.");
+                return;
+            }
+            else{
+                state_message.setTextColor(getResources().getColor(R.color.holo_green_light));
+                state_message.setText("사용 가능한 닉네임입니다.");
+                create_button.setText("생성");
+            }
         }
+    };
+
+    /*back event*/
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (type.equals("NAVER")) mOAuthLoginModule.logout(mContext);
+        else if (type.equals("GOOGLE")) mGoogleSignInClient.signOut();
     }
 }
